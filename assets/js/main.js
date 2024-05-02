e67f4afe670ae35a327ecfff6fd453e7ae1cadbb
sina <sinaa.afshar@gmail.com>
2024-05-02 14:29:16 +0330

    design index page with add some icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,32 @@
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
   
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        document.getElementById('checkbox_text').textContent="فعال سازی حالت روشن";
+
+    }
+    else {        
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        document.getElementById('checkbox_text').textContent="فعال سازی حالت تیره";
+
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
 var $ = jQuery.noConflict();
 
 $(document).ready(function(){
